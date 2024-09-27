ab6933ae428b05a5bd50b5f539c6bd2d7133d61d
Kailash Khadka <k.khadka343@gmail.com>
2024-09-27 19:21:06 +0000

    Ex1 print integer

---
+++ unit6_exercises/include/unit6_exercises/magic_printer.h
@@ -0,0 +1,20 @@
+#ifndef MAGIC_PRINTER_H
+#define MAGIC_PRINTER_H
+
+#include "nav_msgs/Odometry.h"
+#include <ros/ros.h>
+
+using namespace std;
+
+class MagicPrinter {
+public:
+  MagicPrinter(ros::NodeHandle &ros_node);
+  ~MagicPrinter();
+
+  void PrintInteger(const int in_value);
+
+private:
+  ros::NodeHandle *m_ros_node_object;
+};
+
+#endif
---
+++ unit6_exercises/src/main_magic_printer.cpp
@@ -0,0 +1,15 @@
+#include "unit6_exercises/magic_printer.h"
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "magic_print_main_node");
+
+  ros::NodeHandle _n("magic_print_ns");
+
+  MagicPrinter magic_printer_object(_n);
+
+  int value_integer = 42;
+  magic_printer_object.PrintInteger(value_integer);
+
+  return 0;
+}
