5961962a4fc4e9299b30ea69ce9d00f75b569a58
Kailash Khadka <k.khadka343@gmail.com>
2024-09-28 03:24:50 +0000

    Ex6_1 Topic subscriber function with Template

--- unit6_exercises/include/unit6_exercises/topic_subscriber_function.h
+++ unit6_exercises/include/unit6_exercises/topic_subscriber_function.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_SUBSCRIBER_FUNCTION_H
 #define MAIN_SUBSCRIBER_FUNCTION_H
 
+#include "ros/node_handle.h"
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <string>
@@ -11,10 +12,43 @@ class TopicSubscriberFuntion {
   std::string subscriber_topic;
 
 public:
-  TopicSubscriberFuntion(ros::NodeHandle &node_handle, const std::string topic);
-  ~TopicSubscriberFuntion();
+  TopicSubscriberFuntion() { ROS_INFO("TopicSubscriberFuntionNode created."); }
+  ~TopicSubscriberFuntion() {
+    delete nh_;
+    ROS_INFO("TopicSubscriberFuntionNode destroyed.");
+  };
 
-  void subscriberCallback(const sensor_msgs::ImageConstPtr &msg);
+  template <typename MessageType>
+  void init(ros::NodeHandle &node_handle, std::string topic) {
+    nh_ = &node_handle;
+    subscriber_topic = topic;
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME,
+                                       ros::console::levels::Info)) {
+      ros::console::notifyLoggerLevelsChanged();
+    }
+    subscriber_ = nh_->subscribe(
+        subscriber_topic, 100,
+        &TopicSubscriberFuntion::subscriberCallback<MessageType>, this);
+    ROS_INFO("Main Topic Subscriber Function Node initialized.");
+  }
+  template <typename MessageType>
+  void subscriberCallback(const typename MessageType::ConstPtr &msg) {
+    ROS_INFO_STREAM(
+        "Call Back Topic Image Data[0]=" << static_cast<int>(msg->data[0]));
+  }
 };
 
 #endif
+/*user:~/catkin_ws/src$ rosmsg show sensor_msgs/Image
+std_msgs/Header header
+  uint32 seq
+  time stamp
+  string frame_id
+uint32 height
+uint32 width
+string encoding
+uint8 is_bigendian
+uint32 step
+uint8[] data
+// the uint8 is an alias of unsigned char, therefore needs casting to int
+*/
--- unit6_exercises/src/main_topic_subscriber_function.cpp
+++ unit6_exercises/src/main_topic_subscriber_function.cpp
@@ -1,10 +1,12 @@
 #include "../include/unit6_exercises/topic_subscriber_function.h"
 #include "ros/node_handle.h"
+#include "sensor_msgs/Image.h"
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "Topic_subscriber_node");
   ros::NodeHandle nh;
-  TopicSubscriberFuntion node(nh, "/camera/rgb/image_raw");
+  TopicSubscriberFuntion topic_subscriber_node;
+  topic_subscriber_node.init<sensor_msgs::Image>(nh, "/camera/rgb/image_raw");
   ros::spin();
 
   return 0;
